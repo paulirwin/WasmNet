3cba57449d2ea20edbaa2a09ff24c770700598fd
Paul Irwin <paulirwin@gmail.com>
2023-10-23 13:24:51 -0600

    Build: Append GITHUB_PATH variable

--- build/wabt.sh
+++ build/wabt.sh
@@ -18,5 +18,6 @@ curl -o wabt/$WABT_TAR -L $WABT_URL
 tar -zxf wabt/$WABT_TAR -C ./wabt --strip-components 1
 
 export PATH="${PATH:+${PATH}:}$PWD/wabt/bin"
+echo "$PWD/wabt/bin" >> $GITHUB_PATH
 wat2wasm --version
 
