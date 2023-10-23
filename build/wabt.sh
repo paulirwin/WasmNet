85ede244f36c35b34296b07e60a12a7189d5d896
Paul Irwin <paulirwin@gmail.com>
2023-10-23 13:11:34 -0600

    Build: Add script to download and extract WABT

---
+++ build/wabt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+DEFAULT_PLATFORM="ubuntu"
+PLATFORM="${1:-$DEFAULT_PLATFORM}"
+
+DEFAULT_WABT_VERSION="1.0.33"
+WABT_VERSION="${2:-$DEFAULT_WABT_VERSION}"
+
+WABT_TAR="wabt-$WABT_VERSION-$PLATFORM.tar.gz"
+WABT_URL="https://github.com/WebAssembly/wabt/releases/download/$WABT_VERSION/$WABT_TAR"
+
+echo "WABT URL: $WABT_URL"
+
+mkdir -p wabt
+curl -o wabt/$WABT_TAR -L $WABT_URL
+tar -zxvf wabt/$WABT_TAR -C ./wabt --strip-components 1
+
+export PATH=$PATH:$PWD/wabt/bin
+
